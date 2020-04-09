6ae90e1e696245043e1b18ba668db1abc3c400a5
Amit Ghetiya <amitghetiya@Amits-MacBook-Pro.local>
2020-04-08 23:52:07 -0400

    Changes to queries

--- Queries.java
+++ Queries.java
@@ -4,6 +4,101 @@ import java.sql.*;
 public class Queries {
 	public static void enter_new_publication(User p) {
 
-		PreparedStatement preps = null;
+		PreparedStatement s1 = null;
+
+		try{
+			p.in.nextLine();
+			System.out.println("Enter following details: %n 1) publication_id %n 2) title %n 3) typical_topics %n 4) type %n 5) periodicity %n %n");
+
+			String publication_id = p.in.nextLine();
+			String title = p.in.nextLine();
+			String typical_topics = p.in.nextLine();
+			String type = p.in.nextLine();
+			String periodicity = p.in.nextLine();
+			
+			s1 = (PreparedStatement) p.conn.prepareStatement("INSERT INTO Publication (publication_id, title, typical_topics, type, periodicity) VALUES (?,?,?,?,?) ");
+			s1.setString(1, publication_id);
+			s1.setString(2, title);
+			s1.setString(3, typical_topics);
+			s1.setString(4, type);
+			s1.setString(5, periodicity);
+
+			if(s1.executeUpdate == 1)
+				System.out.println("Publication Info added");
+			else
+				System.out.println("Sorry the Staff info couldn't be added");
+		}catch(Exception whatever){
+			System.out.println(whatever);
+		}
 	}
+
+	public static void update_publication_info(User p){
+
+		PreparedStatement s2 = null;
+		try{
+			System.out.println("Enter the publication_id of the record to be updated");
+
+			String pub_id = p.in.nextLine();
+			System.out.println("What do you want to update? %n 1) title %n 2) typical_topics %n 3) type %n 4) periodicity %n %n");
+			String ch = p.in.nextLine();
+
+			if (ch == 1) {
+				System.out.println("Enter the new title : ");
+				p.in.nextLine();
+				String title1 = p.in.nextLine();
+
+				s2 = (PreparedStatement) p.conn.prepareStatement("UPDATE Publication SET title = ? WHERE publication_id = ?"); 
+
+				s2.setString(1, title1);
+				s2.setString(2, pub_id);
+				if (s2.executeUpdate() == 1)
+					System.out.println("Publication Info Updated!");
+				else
+					System.out.println("Couldn't update the Publication Info");
+			} else if(ch == 2){
+				System.out.println("Enter the new typical_topics : ");
+				p.in.nextLine();
+				String tt1 = p.in.nextLine();
+
+				s2 = (PreparedStatement) p.conn.prepareStatement("UPDATE Publication SET typical_topics = ? WHERE publication_id = ?"); 
+
+				s2.setString(1, tt1);
+				s2.setString(2, pub_id);
+				if (s2.executeUpdate() == 1)
+					System.out.println("Publication Info Updated!");
+				else
+					System.out.println("Couldn't update the Publication Info");
+			} else if(ch == 3){
+				System.out.println("Enter the new type : ");
+				p.in.nextLine();
+				String type1 = p.in.nextLine();
+
+				s2 = (PreparedStatement) p.conn.prepareStatement("UPDATE Publication SET type = ? WHERE publication_id = ?"); 
+
+				s2.setString(1, type1);
+				s2.setString(2, pub_id);
+				if (s2.executeUpdate() == 1)
+					System.out.println("Publication Info Updated!");
+				else
+					System.out.println("Couldn't update the Publication Info");
+			} else if(ch == 4){
+				System.out.println("Enter the new periodicity : ");
+				p.in.nextLine();
+				String period1 = p.in.nextLine();
+
+				s2 = (PreparedStatement) p.conn.prepareStatement("UPDATE Publication SET periodicity = ? WHERE publication_id = ?"); 
+
+				s2.setString(1, period1);
+				s2.setString(2, pub_id);
+				if (s2.executeUpdate() == 1)
+					System.out.println("Publication Info Updated!");
+				else
+					System.out.println("Couldn't update the Publication Info");
+			}
+		} catch(Exception whatever){
+			System.out.println(whatever);
+		}
+	}
+
+	
 }
--- User.java
+++ User.java
@@ -19,17 +19,17 @@ public class User{
 	public static int choose_role(){
 		int chosen_role = 0;
 		do{
-			System.out.println("Who would you like to log in as?\npress 1 for Admin, 2 for Managers, 3 for Contributor, 4 for Distributor\n");
+			System.out.println("Who would you like to log in as?\npress 1 for Admin, 2 for Managers, 3 for Contributor\n");
 			Scanner in = new Scanner(System.in); 
 			chosen_role = in.nextInt();
-			if(chosen_role >= 1 || chosen_role <= 4){
+			if(chosen_role >= 1 || chosen_role <= 3){
 				System.out.println("Role chosen successfully");
 			}
 			else{
 				System.out.println("Incorrect input. Please enter a number between 1 and 4.");
 			}
 			
-		}while(role < 1 || role > 4);
+		}while(role < 1 || role > 3);
 		return chosen_role;
 	}
 
@@ -42,17 +42,60 @@ public class User{
 			case 2:
 				System.out.println("Hello Managers, what would you like to do ?");
 				// Enter all the operations managers could perform
+				System.out.println("1: Enter new Publication Information");
+				System.out.println("2: Update Publication Information");
+				System.out.println("3: Assign Editors to Publication");
+				
+
+
+
+				System.out.println("6: Enter a new book edition of a Publication");
+				System.out.println("7: Enter a new issue of a Publication");
+				System.out.println("6: Delete book edition of a Publication");
+				System.out.println("7: Delete issue of a Publication");
+
+				System.out.println("8: Enter a new article");
+				System.out.println("9: Update article info");
+				System.out.println("10: Enter article text");
+				System.out.println("11: Update article text");
+				System.out.println("12: Find books by topic, date, author's name");
+				System.out.println("13: Find articles by topic, date, author's name");
+				System.out.println("14: Enter payment info for Contributors");
+				System.out.println("15: Track payment info");
+
+
+
+				System.out.println("2: Enter new Distributor");
+				System.out.println("2: Update Distributor Information");
+				System.out.println("2: Delete a Distributor");
+				System.out.println("2: Enter new order Information for a distributor");
+				System.out.println("2: Bill Distributor for an order");
+				System.out.println("2: Change outstanding balance on receipt of payment");
+
+
+
+				System.out.println("22: Generate montly reports: number and total price of copies of each publication bought per distributor per month");
+				System.out.println("23: Total revenue of publishing house");
+				System.out.println("24: Total expenses of publishing house");
+				System.out.println("25: Get total current number of distributors");
+				System.out.println("25: Get total current number of distributors");
+				System.out.println("26: Calculate total revenue (since inception) per city, per distributor, and per location");
+				System.out.println("27: Calculate total payments to the editors and authors, per time period and per work type");
+
+
+
 				break;
 
 			case 3:
 				System.out.println("Hello Contributors, what are you doing today ?");
 				// Enter all the operations Contributors could perform
+				System.out.println("28: View Publication info Contributor is reponsible for");
+				System.out.println("4: Add Articles to periodic Publication");
+				System.out.println("5: Delete articles from periodic Publication");
+				System.out.println("6: Add Chapters to Book");
+				System.out.println("7: Delete chapters from Book");
 				break;
 
-			case 4:
-				System.out.println("Hello Distributors, select operations from the list below ?");
-				// Enter all the operations Distributors could perform
-				break;
 
 			this.operation = in.nextInt();
 		}
